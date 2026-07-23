b9aefc895438674b66183ea7853672ec1b4ffc28
cal <sakphea05@gmail.com>
2026-07-23 20:59:55 +0700

    feat(index): route homepage to navbar and load to dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello, world");
+import { Navbar } from "./components/Navbar.js";
+import { Home } from "./pages/Home.js";
+
+import "./styles/reset.css";
+import "./styles/global.css";
+
+const content = document.querySelector("#content");
+const clearContent = () => {
+  content.innerHTML = "";
+};
+
+const loadHome = () => {
+  clearContent();
+  content.append(Home());
+};
+
+const navComponent = Navbar(loadHome);
+document.body.append(navComponent, content);
+
+loadHome();
